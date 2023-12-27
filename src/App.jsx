62f10a175baa6f855ef8cde73c4bfe85e73f5f9c
ronak <ronakbisht475@gmail.com>
2023-12-27 21:26:07 +0530

    second

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import axios from 'axios'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+ 
+  useEffect(()=>{
+    async function get(){
+      try{
+        const {data}=await axios('http://15.206.217.196:4000/all-product')
+        console.log(data)
+      }catch(err){
+        console.log(err.message)
+      }
+    }
+    get()
+   
+  },[])
   return (
     <>
       <div>
@@ -28,6 +41,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      
     </>
   )
 }
